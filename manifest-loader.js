b53942b20464561f590ae970b51965cf958f7cf0
divinegamingblogspot-dot <divinegamingblogspot@gmail.com>
2026-09-17 17:50:05 +0530

    Load PWA manifest on the existing portal

---
+++ manifest-loader.js
@@ -0,0 +1,1 @@
+(function(){if(!document.querySelector('link[rel="manifest"]')){var l=document.createElement('link');l.rel='manifest';l.href='./manifest.json?v=20260917';document.head.appendChild(l)}})();
